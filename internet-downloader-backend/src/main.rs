ee646345ecb90e90c29f0c4b80d7b4b4d7a3c0a5
luciano093 <luparedes100@gmail.com>
2025-08-16 02:00:11 -0400

    Replace infinite loop with an actual webserver

--- internet-downloader-backend/src/main.rs
+++ internet-downloader-backend/src/main.rs
@@ -1,7 +1,12 @@
+use std::{process::exit, sync::Arc};
+
+use axum::{extract::{Query, State}, routing::post, Router};
 use internet_downloader_backend::state_manager::StateManager;
 use internet_downloader_backend::download::DownloadManager;
 
-use tokio::{fs::File};
+use reqwest::StatusCode;
+use serde::Deserialize;
+use tokio::{fs::File, signal, sync::Mutex};
 
 #[tokio::main]
 async fn main() {
@@ -19,7 +24,38 @@ async fn main() {
 
     download_manager.start_processing().await;
 
-    loop {
+    let download_manager = Arc::new(Mutex::new(download_manager));
 
-    }
+    let app = Router::new()
+        .route("/add-download", post(add_download))
+        .with_state(download_manager);
+
+    let listener = tokio::net::TcpListener::bind("localhost:3211").await.unwrap();
+
+    tokio::spawn(async move {
+        signal::ctrl_c().await.unwrap();
+
+        println!("Exiting program.");
+
+        exit(0);
+    });
+
+    let addr = listener.local_addr().unwrap();
+    println!("Server started at localhost:{}", addr.port());
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+#[derive(Deserialize, Debug)]
+struct DownloadQuery {
+    url: String,
+}
+
+#[axum::debug_handler] 
+async fn add_download(State(manager): State<Arc<Mutex<DownloadManager>>>, Query(params): Query<DownloadQuery>) -> StatusCode {
+    println!("received: {:?}", params);
+
+    manager.lock().await.add_download(&params.url).await.unwrap();
+    
+    StatusCode::OK
 }
