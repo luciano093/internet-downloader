3561a719fb169d6e7d6d7fba84e9df43f972e5a2
luciano093 <luparedes100@gmail.com>
2026-06-09 07:47:33 -0400

    Make semi-generic plugin file_urls into a completely generic plugin for http/https

--- internet-downloader-backend/plugins/file_urls.js
+++ internet-downloader-backend/plugins/file_urls.js
@@ -3,36 +3,16 @@
 
 /** @type {import("./host").Plugin} */
 export default {
-    supports_regex: ["\.[a-z0-9]{2,5}(?:[?#].*)?$"],
+    supports_regex: ["^https?://"],
 
     async parse(url, utils) {
-
-        /** @type {string} */
-        let file_name = getFileName(url);
-        
         return {
             url: url,
             task_type: {
                 type: "file",
-                file_name: file_name,
+                file_name: null,
                 url: url,
             }
         };
     }
-}
-
-/**
- * @param {string} urlStr
- */
-function getFileName(urlStr) {
-        let path = urlStr.split('?')[0].split('#')[0];
-
-    // Remove trailing slashes (e.g., "site.com/dir/" -> "site.com/dir")
-    if (path.endsWith('/')) {
-        path = path.slice(0, -1);
-    }
-    const parts = path.split('/');
-    const fileName = parts[parts.length - 1];
-
-    return fileName || "file"; 
 }
