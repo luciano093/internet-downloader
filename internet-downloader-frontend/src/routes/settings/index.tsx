bcb1b81faa6f1a5deb5b309d814809b7c30249a7
luciano093 <luparedes100@gmail.com>
2026-08-02 20:54:15 -0400

    Add the settings sidebar menu

--- internet-downloader-frontend/src/routes/settings/index.tsx
+++ internet-downloader-frontend/src/routes/settings/index.tsx
@@ -4,6 +4,7 @@ import { useSettings, useSetGlobalSettings } from '@/stores/settingsStore';
 import { useState } from 'react';
 import { HostRulesTable } from './components/HostRulesTable';
 import EditableBytesLimit from '@/components/EditableLimit';
+import SettingsSidebar from './components/SettingsSidebar';
 
 export const Route = createFileRoute('/settings/')({
   component: Settings,
@@ -19,7 +20,7 @@ function Settings() {
   const globalLimit = settings?.global_speed_limit ?? null;
 
   return (
-    <AppLayout>
+    <AppLayout sidebarTop={<SettingsSidebar />}>
       <div className="flex-1 overflow-auto bg-background">
         <div className="max-w-4xl mx-auto p-6 space-y-8 text-xs">
           
